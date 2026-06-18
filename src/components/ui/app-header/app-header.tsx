139a60ef0446e93c1c9600ee5cd87a4d72d32e03
Natalia Yashina <natasha.yashina.15@gmail.com>
2026-06-18 17:13:43 +0300

    fixes

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -18,51 +18,37 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
   const isFeedActive = pathname.startsWith('/feed');
   const isProfileActive = pathname.startsWith('/profile');
 
+  const linkClassNames = ({ isActive }: { isActive: boolean }) =>
+    clsx(styles.link, 'text', 'text_type_main-default', 'ml-2', 'mr-10', {
+      [styles.link_active]: isActive
+    });
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.menu} p-4`}>
-        {/* Левая часть меню */}
         <div className={styles.menu_part_left}>
           <div className={styles.menu_item}>
             <BurgerIcon type={isConstructorActive ? 'primary' : 'secondary'} />
-            <NavLink
-              to='/'
-              end
-              className={({ isActive }) =>
-                clsx(styles.link, { [styles.link_active]: isActive })
-              }
-            >
+            <NavLink to='/' end className={linkClassNames}>
               Конструктор
             </NavLink>
           </div>
 
           <div className={styles.menu_item}>
             <ListIcon type={isFeedActive ? 'primary' : 'secondary'} />
-            <NavLink
-              to='/feed'
-              className={({ isActive }) =>
-                clsx(styles.link, { [styles.link_active]: isActive })
-              }
-            >
+            <NavLink to='/feed' className={linkClassNames}>
               Лента заказов
             </NavLink>
           </div>
         </div>
 
-        {/* Центр логотип */}
         <div className={styles.logo}>
           <Logo className='' />
         </div>
 
-        {/* Правая часть меню */}
         <div className={styles.link_position_last}>
           <ProfileIcon type={isProfileActive ? 'primary' : 'secondary'} />
-          <NavLink
-            to='/profile'
-            className={({ isActive }) =>
-              clsx(styles.link, { [styles.link_active]: isActive })
-            }
-          >
+          <NavLink to='/profile' className={linkClassNames}>
             {userName || 'Личный кабинет'}
           </NavLink>
         </div>
